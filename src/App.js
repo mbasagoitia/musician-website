ff1c3c41c3e0872c20a4ec098e7f99c616532582
basagoitiam <marika.basagoitia@gmail.com>
2024-01-23 15:06:06 -0500

    edited styling of home page so text doesnt cover face; added parallax scroll

--- src/App.js
+++ src/App.js
@@ -12,79 +12,71 @@ function App() {
     backgroundColor: 'transparent'
   });
 
-// useEffect for handling scroll events
-useEffect(() => {
-  const handleScroll = debounce(() => {
-    const bioSection = document.getElementById('bio');
-    const bioSectionRect = bioSection.getBoundingClientRect();
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      const bioSection = document.getElementById('bio');
+      const bioSectionRect = bioSection.getBoundingClientRect();
 
-    if (bioSectionRect.top > 0 && !navbarExpanded) {
-      // Set transparent background when the user has not scrolled past the bio section and navbar is closed
+      if (bioSectionRect.top > 0 && !navbarExpanded) {
+        setNavbarStyle({
+          top: '0',
+          left: '0',
+          right: '0',
+          zIndex: '1000',
+          backgroundColor: 'transparent',
+        });
+      } else if (bioSectionRect.top > 0 && navbarExpanded) {
+        setNavbarStyle({
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          right: '0',
+          zIndex: '1000',
+          backgroundColor: 'gray',
+        });
+      } else if (bioSectionRect.top <= 0) {
+        setNavbarStyle({
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          right: '0',
+          zIndex: '1000',
+          backgroundColor: 'gray',
+        });
+      }
+    }, 100);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [navbarExpanded, setNavbarStyle]);
+
+  useEffect(() => {
+    if (!navbarExpanded) {
       setNavbarStyle({
         position: 'fixed',
         top: '0',
         left: '0',
         right: '0',
-        zIndex: '1000', // Adjust z-index as needed
+        zIndex: '1000',
         backgroundColor: 'transparent',
       });
-    } else if (bioSectionRect.top > 0 && navbarExpanded) {
-      // Set gray background when the user has scrolled past the bio section or navbar is open
+    } else {
       setNavbarStyle({
         position: 'fixed',
         top: '0',
         left: '0',
         right: '0',
-        zIndex: '1000', // Adjust z-index as needed
-        backgroundColor: 'gray',
-      });
-    } else if (bioSectionRect.top <= 0) {
-      // Set gray background when the user has scrolled past the bio section or navbar is open
-      setNavbarStyle({
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        right: '0',
-        zIndex: '1000', // Adjust z-index as needed
+        zIndex: '1000',
         backgroundColor: 'gray',
       });
     }
-  }, 100);
+  }, [navbarExpanded, setNavbarStyle]);
 
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, [navbarExpanded, setNavbarStyle]);
-
-// useEffect for handling changes in navbarExpanded state
-useEffect(() => {
-  if (!navbarExpanded) {
-    // Set transparent background when the navbar is closed
-    setNavbarStyle({
-      position: 'fixed',
-      top: '0',
-      left: '0',
-      right: '0',
-      zIndex: '1000', // Adjust z-index as needed
-      backgroundColor: 'transparent',
-    });
-  } else {
-    // Set gray background when the navbar is open
-    setNavbarStyle({
-      position: 'fixed',
-      top: '0',
-      left: '0',
-      right: '0',
-      zIndex: '1000', // Adjust z-index as needed
-      backgroundColor: 'gray',
-    });
-  }
-}, [navbarExpanded, setNavbarStyle]);
-
-  
-  
+    
+    
 
   return (
     <div className="App">
--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-function Home () {
+function Home() {
     return (
-    <div id="home">
-       <div className="overlay-container">
-        <div className="hp-content-wrapper">
-          <h1 id="hp-title">Belén Hernández</h1>
-          <h2 id="hp-subtitle">Violinist/Violin Teacher</h2>
+      <div id="home" className="fixed-background">
+        <div className="overlay-container">
+          <div className="hp-content-wrapper">
+            <h1 id="hp-title">Belén Hernández</h1>
+            <h2 id="hp-subtitle">Violinist/Violin Teacher</h2>
+          </div>
         </div>
       </div>
-    </div>
-    )
-}
+    );
+  }
 
 export default Home;
