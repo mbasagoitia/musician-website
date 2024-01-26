87bcba00aae37d2fdecaa81fcc4b2b5f22ce51b6
basagoitiam <marika.basagoitia@gmail.com>
2024-01-26 13:35:22 -0500

    rendered home page md content using front matter

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -1,14 +1,61 @@
+import { useEffect, useState } from 'react';
+import fm from 'front-matter';
+
 function Home() {
-    return (
-      <div id="home" className="fixed-background">
-        <div className="overlay-container">
-          <div className="hp-content-wrapper">
-            <h1 id="hp-title">Belén Hernández</h1>
-            <h2 id="hp-subtitle">Violinist/Violin Teacher</h2>
-          </div>
+
+  const [pageContent, setPageContent] = useState({});
+
+  useEffect(() => {
+    fetch('/content/pages/home.md')
+      .then((response) => response.text())
+      .then((markdownContent) => {
+        const parsedContent = fm(markdownContent);
+        setPageContent(parsedContent.attributes);
+      });
+  }, []);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isWideScreen = windowWidth >= 520;
+
+  return (
+    <div id="home" className={`fixed-background`}>
+      <div
+        style={{
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundImage: `url(${pageContent.backgroundImage})`,
+          backgroundPosition: '60%',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundAttachment: isWideScreen ? 'fixed' : 'scroll'
+        }}
+      />
+      <div className="overlay-container">
+        <div className="hp-content-wrapper">
+          <h1 className="hp-title">{pageContent.hpTitle}</h1>
+          <h2 className="hp-subtitle">{pageContent.hpSubtitle}</h2>
         </div>
       </div>
-    );
+    </div>
+  );
+  
   }
 
 export default Home;
